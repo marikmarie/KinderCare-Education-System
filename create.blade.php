6320f7a7a74a7f1487d13ece608d9b40575fe687
Nyamwizadebrah <95640003+Nyamwizadebrah@users.noreply.github.com>
2022-02-15 14:59:15 +0300

    Add files via upload

---
+++ create.blade.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>KinderCare Character Drawing System - KCDS</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{asset('plugins/jqvmap/jqvmap.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{asset('plugins/daterangepaicker/daterangepicker.css')}}">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+    @include('navbars.auth.auth')
+    @include('sidebars.leftsidebar')
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">KCDS</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- error part -->
+
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+  <strong>Whoops!</strong> There were some problems with your input.<br><br>
+  <ul>
+     @foreach ($errors->all() as $error)
+       <li>{{ $error }}</li>
+     @endforeach
+  </ul>
+</div>
+@endif
+        <!--error part-->
+        <div class="row">
+          <!--form add user -->
+          <div class="register-box m-auto col-md-8">
+            <div class="card card-outline card-success">
+              <div class="card-header text-center">
+                <h2>Add Assignment</h2>
+              </div>
+              <div class="card-body">
+                <p class="login-box-msg">Register a new Assignment</p>
+
+
+                <form method="POST" action="{{route('assignment.store')}}">
+                    @csrf
+                    <div class="form-group mb-3">
+
+                        <label for=""> Name</label>
+                        <input type="text" name="name" required placeholder="enter assignment  name" class="form-control"/>
+                       </div>
+                       <div class="form-group mb-3">
+
+                        <label for="">Assignment  Characters</label>
+
+                        <textarea name="characters"  required placeholder="enter assignment  characters" class="form-control" 
+                         maxlength="8"></textarea>
+                       </div>
+
+                       <div class="form-group mb-3">
+
+                         <label for="">Date of Attempt</label>
+                         <input type="date" name="dateOfAttempt" required placeholder="enter date of attempt" class="form-control"/>
+
+                        </div>
+
+                        <div class="form-group mb-3">
+
+                         <label for="">Start Time</label>
+                         <input type="time" name="startTime" required placeholder="enter start time" class="form-control"/>
+                        </div>
+
+                        <div class="form-group mb-3">
+
+                         <label for="">End Time</label>
+                         <input type="time" name="endTime" required placeholder="enter end time" class="form-control"/>
+                        </div>
+
+
+
+                       <div class="row">
+
+                         <!-- /.col -->
+                         <div class="col-12">
+                           <button type="submit" class="btn  btn-block btn-primary btn-success">Add Assignment</button>
+                         </div>
+                         <!-- /.col -->
+                       </div>
+
+
+                </form>
+
+
+
+              </div>
+              <!-- /.form-box -->
+            </div><!-- /.card -->
+          </div>
+          <!-- /.register-box -->
+
+          <!--form add user-->
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-7 connectedSortable">
+
+
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  @include('footer.footer')
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{  asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{ asset('plugins/chart.js/Chart.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{ asset('plugins/sparklines/sparkline.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{ asset('plugins/moment/moment.min.js')}}"></script>
+<script src="{{  asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- Summernote -->
+<script src="{{ asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{  asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{  asset('dist/js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{  asset('dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('dist/js/pages/dashboard.js')}}"></script>
+</body>
+</html>
---
+++ index.blade.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KinderCare Character Drawing System - KCDS</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <style>
+        .style_button {
+            background: #657836 !important;
+            color: #fff;
+            width: 100% !important;
+            border: none !important;
+            height: 40px !important;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+
+
+    </style>
+    <style>
+        .style_button {
+            background: #657836 !important;
+            color: #fff;
+            width: 100% !important;
+            border: none !important;
+            height: 40px !important;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+
+        .farmer__filter {
+            display: flex !important;
+            align-items: center !important;
+            justify-content: space-evenly !important;
+        }
+        .platform{
+            display: none !important;
+        }
+
+        .content-wrapper{
+            position: relative !important;
+        }
+        .image__remove{
+            position: absolute !important;
+            right: 30px !important;
+            top:10px !important;
+            cursor: pointer;
+        }
+
+    </style>
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        @include('navbars.auth.auth')
+        <!-- Main Sidebar Container -->
+        @include('sidebars.leftsidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <!--any wrong info-->
+            <div id="content-wrap">
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success m-4" >
+                    <p>{{ $message }}</p>
+                    <img src="{{asset('images/remove.png')}}"
+             class="image__remove"
+
+            alt="cross image"
+             height="20px"
+             width="20px"
+
+            >
+
+                </div>
+            @endif
+            <!--any wrong info-->
+            </div>
+            <!--any wrong info-->
+
+            <!-- /.card -->
+            <!-- Content Header (Page header) -->
+                         ___THE ASSIGNMENT PAGE
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Assigments</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="{{ route('assignment.index') }}">Home</a></li>
+                                <li class="breadcrumb-item active">Assigments</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+
+
+                    <div class="row">
+                        <div class="col-12">
+                            <!--table-->
+                            <!-- /.card -->
+
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title ">Assigments</h3>
+                                    <h4 class="float-sm-right ">
+                                        <a class="btn btn-success" href="{{ route('assignment.create') }}"> Add New Assignment
+                                            </a>
+                                    </h4>
+                                </div>
+
+                                <!--table removed-->
+                                <div class="card-body">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>AssignmentName</th>
+                                                <th>Date </th>
+                                                <th>Start Time</th>
+
+                                                <th>End Time</th>
+                                                <th>Characters</th>
+
+                                                <th>Assignment Due</th>
+                                                
+
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <!--create a tr--->
+
+                                            @foreach ($data as $key => $user)
+                                                <tr>
+                                                    <td>{{ $user->name }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $user->dateOfAttempt}}
+                                                    </td>
+                                                    <td>
+
+                                                        {{$user->startTime}}
+                                                    </td>
+
+                                                    <td>
+                                                        {{$user->endTime}}
+                                                    </td>
+                                                    <td>{{$user->url}}</td>
+                                                       
+
+
+                                                    <td>
+                                                        
+                                                       @if ($user->assignmentDue == TRUE)
+                                                       Yes
+                                                  @else
+                                                       No
+                                                  @endif
+                                                    </td>
+
+
+                                                </tr>
+                                            @endforeach
+                                            <!--create a tr--->
+
+                                        </tbody>
+
+                                    </table>
+                                </div>
+
+
+                                <!--table removed-->
+                            </div>
+                            <!-- /.card -->
+                            <!--table-->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        @include('footer.footer')
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('dist/js/demo.js') }}"></script>
+    <!-- Page specific script -->
+
+
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+    </script>
+
+        <!--hide alert--->
+        <script type="text/javascript">
+            $(function(){
+                $('.image__remove').click(function(){
+                    //alert('clicked')
+                    $("#content-wrap").addClass('platform');
+                    $("..image__remove").addClass('platform');
+
+                })
+            })
+           </script>
+           <!--hide alert-->
+
+
+</body>
+
+</html>
